b2f22ee3ccf4d4c58d7b781390cbd3bee492321c
ShameenRoopasingha <dinukaroopasingha@gmail.com>
2026-06-18 16:07:22 +0530

    feat: implement Login page and integrate it into the application shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Layout } from "./layout/layout";
 import { SignUp } from "./pages/SignUp";
+import { Login } from "./pages/Login";
 
 /**
  * Root application component.
  *
- * Currently renders the Sign Up page inside the shared Layout shell.
+ * Currently renders the Login page inside the shared Layout shell.
  * Once react-router is added, individual pages will be routed here.
  */
 export default function App() {
   return (
     <Layout>
-      <SignUp />
+      <Login />
     </Layout>
   );
 }
